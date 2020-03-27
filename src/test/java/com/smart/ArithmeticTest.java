b415e76fdb91724a2f5248349e20edfb73e879f6
1721819634 <boyuzhang123@163.com>
2020-03-27 15:03:16 +0800

    add testOperate

--- src/test/java/com/smart/ArithmeticTest.java
+++ src/test/java/com/smart/ArithmeticTest.java
@@ -1,7 +1,10 @@
 package com.smart;
 
 
+import com.smart.entity.Question;
+import com.smart.util.IArithmeticUtil;
 import com.smart.util.IProduceQuestionUtil;
+import com.smart.util.impl.ArithmeticUtilImpl;
 import com.smart.util.impl.ProduceQuestionUtilImpl;
 import org.junit.Test;
 
@@ -20,5 +23,16 @@ public class ArithmeticTest {
         for (int i = 0; i < 10; i++) {
             System.out.println(produceQuestionUtil.produce(10));
         }
+    } 
+    
+    @Test
+    public void testOpera() {
+        IArithmeticUtil arithmeticUtil = new ArithmeticUtilImpl();
+        String suffix = "8'1/20 2'1/2 ÷ 8'1/6 * 1 *";
+        Question question = new Question();
+        question.setSuffixQuestion(suffix);
+        arithmeticUtil.operate(question);
+        System.out.println(question);
+        
     }
 }
