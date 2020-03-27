2fadf8c1a81340f883007ab695bba8c90e0e2775
JackyLin18 <2563269611@qq.com>
2020-03-27 23:26:44 +0800

    ..

--- src/main/java/com/smart/gui/ArithmeticFrame.java
+++ src/main/java/com/smart/gui/ArithmeticFrame.java
@@ -12,7 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
-import java.net.InterfaceAddress;
 import java.util.ArrayList;
 import java.util.List;
 
